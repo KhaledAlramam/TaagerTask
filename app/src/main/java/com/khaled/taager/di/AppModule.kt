05e29a14624ed16d26e31c9c3c3328fa04c53b51
KhaledAlramam <khaledalramam>
2021-08-10 15:06:32 +0200

    - Add Remaining hilt module setup

--- app/src/main/java/com/khaled/taager/di/AppModule.kt
+++ app/src/main/java/com/khaled/taager/di/AppModule.kt
@@ -7,21 +7,18 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
-import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
     @Provides
-    @Singleton
     fun provideRetrofit(): Retrofit = Retrofit.Builder()
         .baseUrl(ApiService.BASE_URL)
         .addConverterFactory(MoshiConverterFactory.create())
         .build()
 
     @Provides
-    @Singleton
     fun provideApi(retrofit: Retrofit): ApiService =
         retrofit.create(ApiService::class.java)
 }
